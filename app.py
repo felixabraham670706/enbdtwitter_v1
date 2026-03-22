3cd351a19a606be67f1788889b1ad8158b84fd03
felixabraham670706 <felixabraham670706@gmail.com>
2026-03-22 19:11:50 +0400

    clean initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import os
+from datetime import datetime
+from streamlit_autorefresh import st_autorefresh
+import pytz
+
+st.set_page_config(page_title="ENBD Twitter Dashboard", layout="wide")
+st_autorefresh(interval=60000)
+
+file_time = os.path.getmtime("twiter1.csv")
+
+
+dubai = pytz.timezone("Asia/Dubai")
+last_update = datetime.fromtimestamp(file_time, dubai)
+st.write("Last data update:", last_update.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+#st.title("Emirates NBD Reddit Sentiment Dashboard")
+
+html_file = "X-post_analysis.html"
+
+if os.path.exists(html_file):
+    
+    with open(html_file, "r", encoding="utf-8") as f:
+        html_content = f.read()
+
+    st.components.v1.html(
+        html_content,
+        height=1200,
+        scrolling=True
+    )
+
+else:
+    st.warning("Report not generated yet.")
